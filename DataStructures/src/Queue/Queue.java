2629b598be7ca0d1707c5f83a6fe2040e040ab09
Piyush Gupta <opacuspiyush@gmail.com>
2016-08-23 13:29:55 -0700

    "QUEUE"

---
+++ DataStructures/src/Queue/Queue.java
@@ -0,0 +1,75 @@
+package Queue;
+
+public class Queue {
+    int front, rear, size,capacity;
+    int[] data;
+
+ public Queue (int capacity){
+    data = new int[capacity];
+    this.capacity = capacity;
+    size = 0;
+    front = 0;
+    rear = capacity - 1;
+}
+
+public boolean IsEmpty(){
+    return size == 0;
+}
+
+public boolean IsFull(){
+    return size == capacity;
+} 
+public void Enqueue(int value){
+    if(IsFull())
+        return;
+    
+    rear = (rear+1)%capacity;
+    size = size +1;
+    data[rear] = value;
+    System.out.println("Inserted Item = " + value);
+}    
+
+public int Dequeue()
+{
+    if(IsEmpty())
+        return Integer.MIN_VALUE;
+     
+    int value = data[front];
+    data[front] = -1;
+    front = (front + 1)%capacity;
+    size = size - 1;
+    
+    System.out.println("Deleted Item = " + value);
+    
+    return value;
+    
+}
+
+public int Front(){
+    if(IsEmpty())
+        return Integer.MIN_VALUE;
+        
+     return data[front];
+}
+
+public int Rear(){
+     if(IsEmpty())
+        return Integer.MIN_VALUE;
+        
+     return data[rear];
+}
+
+public static void main(String[] args){
+	
+	Queue queue = new Queue(5);
+	
+	queue.Enqueue(1);
+	
+	System.out.println("Value " + queue.Dequeue());
+	
+	queue.Enqueue(5);
+	
+}
+}
+
+
