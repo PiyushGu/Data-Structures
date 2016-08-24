21c7af71befbe0bde150cadd3e6ad3a84cc1c3ec
Piyush Gupta <opacuspiyush@gmail.com>
2016-08-23 21:51:06 -0700

    "Queue using Linked List"

--- DataStructures/src/Queue/Queue_LinkedList.java
+++ DataStructures/src/Queue/Queue_LinkedList.java
@@ -2,16 +2,57 @@ package Queue;
 
 public class Queue_LinkedList {
 
-	Queue_Node front,rear;
-	
-	public Queue_LinkedList(){
-		front = new Queue_Node();
-		rear = new Queue_Node();
-		
-	}
-	
-	public void Enqueue(int value){
-		
-	}
-	
+    Queue_Node front,rear;
+    
+    public void Enqueue(int value)
+    {
+        Queue_Node node = new Queue_Node(value);
+        
+        if(front == null)
+        {
+            front = rear = node;
+            return;
+        }
+        
+        rear.next = node;
+        rear = node;
+        
+    }
+    
+    public int Dequeue()
+    {
+        if (front == null)
+            return -1;
+         
+        Queue_Node temp;
+        
+        temp = front;
+        
+        front = front.next;
+        
+        if(front == null)
+            rear = null;
+           
+        return temp.value;
+    }
+    
+    public static void main(String[] args)
+    {
+        Queue_LinkedList queue = new Queue_LinkedList();
+        
+        queue.Enqueue(10);
+        
+        System.out.println("Value = " + queue.Dequeue());
+        
+        queue.Enqueue(30);
+        
+        queue.Enqueue(20);
+        
+        queue.Enqueue(40);
+        
+        System.out.println("Value = " + queue.Dequeue());
+        
+        
+    }
+    
 }
