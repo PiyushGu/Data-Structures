9b8270f0233413007d52ab1f43352db39ae974d0
Piyush Gupta <opacuspiyush@gmail.com>
2016-09-04 20:36:23 -0700

    "Change in Serialize"

--- DataStructures/src/Miscellaneous/SerializeAndDeserializeTree.java
+++ DataStructures/src/Miscellaneous/SerializeAndDeserializeTree.java
@@ -59,7 +59,4 @@ public class SerializeAndDeserializeTree {
 	}
 }
 
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.deserialize(codec.serialize(root));
 
